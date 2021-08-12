af9dd45cace959719675cc141ae5e5b3f2102b1d
Kevin Rondon <kevin.rondon@uao.edu.co>
2021-08-11 23:43:04 -0500

    W-1: First scene improve

---
+++ Assets/Scripts/GUI/AnimatorController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorController : MonoBehaviour
+{
+    /// <summary>
+    /// Animator necessary to control states
+    /// </summary>
+    Animator animator;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Method to controll boolean parameters in animator
+    /// </summary>
+    /// <param name="anim">Boolean parameter of animator</param>
+    public void SetBoolAnim(string anim){
+        bool state = animator.GetBool(anim);
+        animator.SetBool(anim, !state);
+    }
+}
