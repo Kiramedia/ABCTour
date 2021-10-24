f520b12f46a0619812224e9637e92d497d18a230
Lesly Lucumi <lesly.lucumi@veevart.com>
2021-10-24 12:57:57 -0500

    Sign and letter

--- Assets/Resources/Graphics/UI/Prefabs/MemoryCard/MemoryCardBehavior.cs
+++ Assets/Resources/Graphics/UI/Prefabs/MemoryCard/MemoryCardBehavior.cs
@@ -9,8 +9,11 @@ public class MemoryCardBehavior : MonoBehaviour
     public Sprite frontImage;
     public Image image;
     public GameObject frontElement;
+    public Color color;
+    public Sign sign;
 
     private bool isFront;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,9 +40,13 @@ public class MemoryCardBehavior : MonoBehaviour
     public void showFront()
     {
         image.sprite = frontImage;
+        image.color = color;
+        frontElement.SetActive(true);
     }
     public void showBack()
     {
         image.sprite = backImage;
+        image.color = new Color32(255, 255, 255, 255);
+        frontElement.SetActive(false);
     }
 }
