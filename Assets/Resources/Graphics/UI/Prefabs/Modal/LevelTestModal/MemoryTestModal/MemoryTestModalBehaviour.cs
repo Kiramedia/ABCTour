bd3263ebcc6295b67edf81b961fcc71fe34435f5
Andres Camilo Arcila <acarcila98@gmail.com>
2021-10-24 23:47:27 -0500

    memory test modal

--- Assets/Resources/Graphics/UI/Prefabs/Modal/LevelTestModal/MemoryTestModal/MemoryTestModalBehaviour.cs
+++ Assets/Resources/Graphics/UI/Prefabs/Modal/LevelTestModal/MemoryTestModal/MemoryTestModalBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class SelectMemoryOptions : MonoBehaviour
+public class MemoryTestModalBehaviour : MonoBehaviour
 {
     public int numberOfOptions;
     public Abecedary levelSigns;
